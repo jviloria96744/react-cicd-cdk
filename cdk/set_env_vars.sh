9d2f68758ce7c1c941f922f5cc7edea737942fd7
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 14:20:42 -0700

    Wrap jq command to set as remove_quotes parameter

--- cdk/set_env_vars.sh
+++ cdk/set_env_vars.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "::set-env name=STACK::$(jq keys_unsorted[] cdk-deploy-output.json)"
-export STACK=$(jq keys_unsorted[] cdk-deploy-output.json) 
+export FILE=cdk-deploy-output.json
+echo "::set-env name=STACK::$(jq keys_unsorted[] $FILE)"
+export STACK=$(jq keys_unsorted[] $FILE) 
 
 function remove_quotes {
     echo $1 | tr -d '"'
 }
 
-echo "::set-env name=DIST_ID::$(remove_quotes jq .$STACK.distid cdk-deploy-output.json)"
-echo "::set-env name=BUCKET::$(remove_quotes jq .$STACK.sitebucketname cdk-deploy-output.json)"
-echo "::set-env name=ARTIFACT_BUCKET::$(remove_quotes jq .$STACK.artifactbucketname cdk-deploy-output.json)"
+echo "::set-env name=DIST_ID::$(remove_quotes $(jq .$STACK.distid $FILE))"
+echo "::set-env name=BUCKET::$(remove_quotes $(jq .$STACK.sitebucketname $FILE))"
+echo "::set-env name=ARTIFACT_BUCKET::$(remove_quotes $(jq .$STACK.artifactbucketname $FILE))"
