3c8d77825158d52ab802f3c5f951e6c71e120a23
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 12:18:56 -0700

    Comment out bucket name line because of old name in json file

--- cdk/set_env_vars.sh
+++ cdk/set_env_vars.sh
@@ -2,7 +2,7 @@
 
 echo "::set-env name=STACK::$(jq keys_unsorted[] cdk-deploy-output.json)"
 echo "::set-env name=DIST_ID::$(jq .[$STACK]['distid'] cdk-deploy-output.json)"
-echo "::set-env name=BUCKET::$(jq .[$STACK]['sitebucketname'] cdk-deploy-output.json)"
+#echo "::set-env name=BUCKET::$(jq .[$STACK]['sitebucketname'] cdk-deploy-output.json)"
 
 echo $STACK
 echo $DIST_ID
