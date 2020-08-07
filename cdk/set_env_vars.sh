ede0ce5d0e1696ba213373e452adba16b76cbf13
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 14:12:07 -0700

    add function to remove quotes from cdk outputs

--- cdk/set_env_vars.sh
+++ cdk/set_env_vars.sh
@@ -2,6 +2,11 @@
 
 echo "::set-env name=STACK::$(jq keys_unsorted[] cdk-deploy-output.json)"
 export STACK=$(jq keys_unsorted[] cdk-deploy-output.json) 
-echo "::set-env name=DIST_ID::$(jq .$STACK.distid cdk-deploy-output.json)"
-echo "::set-env name=BUCKET::$(jq .$STACK.sitebucketname cdk-deploy-output.json)"
-echo "::set-env name=ARTIFACT_BUCKET::$(jq .$STACK.artifactbucketname cdk-deploy-output.json)"
+
+function remove_quotes {
+    echo $1 | tr -d '"'
+}
+
+echo "::set-env name=DIST_ID::$(remove_quotes jq .$STACK.distid cdk-deploy-output.json)"
+echo "::set-env name=BUCKET::$(remove_quotes jq .$STACK.sitebucketname cdk-deploy-output.json)"
+echo "::set-env name=ARTIFACT_BUCKET::$(remove_quotes jq .$STACK.artifactbucketname cdk-deploy-output.json)"
