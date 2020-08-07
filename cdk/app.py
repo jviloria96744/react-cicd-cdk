abd131e3e1815bc726e173cb1fc65b41612e4874
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 11:06:22 -0700

    set testing variable in workflow to skip react dependency install step

--- cdk/app.py
+++ cdk/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-
+import os
 from aws_cdk import core
-
 from static_site_stack.static_site_stack import StaticSiteStack
 
 app = core.App()
 
-env = app.node.try_get_context("environment")
+environment = app.node.try_get_context("environment")
 domain = app.node.try_get_context("domain")
 
-stack_id = f"StaticSiteStack-{env}"
-StaticSiteStack(app, stack_id, env, domain)
+aws_env = core.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
+                           region=os.environ["CDK_DEFAULT_REGION"])
+
+stack_id = f"StaticSiteStack-{environment}"
+StaticSiteStack(app, stack_id, environment, domain, env=aws_env)
 
 app.synth()
