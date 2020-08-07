ff8a523255bbcf812597096befd20a6674e7ff54
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 12:42:50 -0700

    Fix typo in sh script to set env variables

--- cdk/set_env_vars.sh
+++ cdk/set_env_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "::set-env name=STACK::$(jq keys_unsorted[] cdk-deploy-output.json)"
-export $STACK=$(jq keys_unsorted[] cdk-deploy-output.json) 
+export STACK=$(jq keys_unsorted[] cdk-deploy-output.json) 
 echo "::set-env name=DIST_ID::$(jq .$STACK.distid cdk-deploy-output.json)"
 echo "::set-env name=BUCKET::$(jq .$STACK.sitebucketname. cdk-deploy-output.json)"
 echo "::set-env name=ARTIFACT_BUCKET::$(jq .$STACK.artifactbucketname cdk-deploy-output.json)"
