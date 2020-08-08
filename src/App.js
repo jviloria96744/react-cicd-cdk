a0811c253cc54e8b1b7e066da08c513b6a252532
Jay Viloria <jviloria96744@gmail.com>
2020-08-07 19:16:36 -0700

    Add items in workflow for STG deployment and get App.js ready for release

--- src/App.js
+++ src/App.js
@@ -18,10 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <p>
-          This is a test feature that shouldn't be deployed until merging into
-          master
-        </p>
+        <p>This paragraph is ready to go to STG</p>
       </header>
     </div>
   );
